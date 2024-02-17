03e8a637dd3c3c4243bbc2f0bb86e1bef92051b1
monthyespino <juanespino524@gmail.com>
2024-02-17 15:51:41 -0500

    Speech Test

--- test#1-Speech.py
+++ test#1-Speech.py
@@ -3,7 +3,7 @@ import os, json
 
 path = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(path, "params.json", 'r')) as fp:
+with open(os.path.join(path, "params.json"), 'r') as fp:
     params = json.load(fp)
 
 params = params["params"]
