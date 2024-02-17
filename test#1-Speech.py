2372daf44e96fc68f8829ec862e3f4f8b4cc8fbf
monthyespino <juanespino524@gmail.com>
2024-02-17 15:40:45 -0500

    Params

--- test#1-Speech.py
+++ test#1-Speech.py
@@ -1,5 +1,15 @@
 from naoqi import ALProxy #? Library Connection 
+import os, json
+
+path = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(path, "params.json", 'r')) as fp:
+    params = json.load(fp)
+
+params = params["params"]
+ip = params["robot_ip"]
+port = params["robot_port"]
 
 #! ALTextToSpeech Function:
-tts = ALProxy("ALTextToSpeech", "127.0.0.1", 59730)
+tts = ALProxy("ALTextToSpeech", ip, port)
 tts.say("hello")
